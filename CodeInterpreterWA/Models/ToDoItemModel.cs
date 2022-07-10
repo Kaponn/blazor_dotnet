9504e6db6e33fd74a5a69a46d105af3431923c63
Kacper Ledwon <you@example.com>
2022-07-10 15:30:52 +0200

    Initial commit

---
+++ CodeInterpreterWA/Models/ToDoItemModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeInterpreterWA.Models
+{
+    public class ToDoItemModel
+    {
+        [StringLength(20, ErrorMessage = "Title is too long. Max 20 characters")]
+
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
